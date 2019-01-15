8a0c9e7a61800e01df044e5e7c4b0d1ff0ab2957
mr21xm <missalqubaisi123@gmail.com>
2019-01-15 13:55:19 +0400

    fixed the mapbox api

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<pk.eyJ1IjoibXIyMXhtIiwiYSI6ImNqcXhrcWRnazBjNjI0N29hZjZ5YmV6dGEifQ.zoPvTOTNum2LwWnWDQzDSA>',
+    mapboxToken: 'pk.eyJ1IjoibXIyMXhtIiwiYSI6ImNqcXhrcWRnazBjNjI0N29hZjZ5YmV6dGEifQ.zoPvTOTNum2LwWnWDQzDSA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
