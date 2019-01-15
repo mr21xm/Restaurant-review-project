fad91e41ddd0c7926b9b3bc600a91197c2a83fd6
mr21xm <missalqubaisi123@gmail.com>
2019-01-15 13:50:57 +0400

    added leaflet library

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -18,7 +18,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<pk.eyJ1IjoibXIyMXhtIiwiYSI6ImNqcXA3bGNvZzBibzkzeG00MHozeTA3bmYifQ.bAvb2BHC4dA9tf8fpN0b4A>',
+        mapboxToken: '<pk.eyJ1IjoibXIyMXhtIiwiYSI6ImNqcXhrcWRnazBjNjI0N29hZjZ5YmV6dGEifQ.zoPvTOTNum2LwWnWDQzDSA>',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
